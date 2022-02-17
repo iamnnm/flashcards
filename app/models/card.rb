74cce1c8c7d94de7671c459c179f2285863692f2
iamnnm <gvozdeffand@yandex.ru>
2022-02-17 10:44:51 +0300

    Task 3.2

--- app/models/card.rb
+++ app/models/card.rb
@@ -12,11 +12,11 @@ class Card < ApplicationRecord
 
   # Adding a date of creation or renewed date of changing cards info
   def set_or_update_review_date
-    if self.review_date == nil
-      self.review_date = DateTime.now
-    else
-      self.review_date = review_date.next_day(3)
-    end
+    self.review_date = if review_date.nil?
+                         DateTime.now
+                       else
+                         review_date.next_day(3)
+                       end
   end
 
   def original_and_translated_texts_cannot_be_similar
